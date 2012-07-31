e71b46170f84939938ddfbfa0050bdff75531622
Makis Tracend <makis.tracend@gmail.com>
2012-07-31 01:05:45 -0700

    Introducing (optional) re-routing based on the URI

--- app/controllers.js
+++ app/controllers.js
@@ -2,14 +2,32 @@
 
 	// Namespace definition
 	APP = {
-		init: {}, 
+		init : function(){},
+		Routers : {},
 		Collections: {},
 		Models: {},
 		Views: {}
 	}
 	
 	// App contructor
-	APP.init = Backbone.Router.extend({
+	APP.init = function(){
+		var router = false;
+		// check URIs
+		var path = window.location.pathname.split( '/' );
+		// find a router based on the path
+		for( i in path ){
+			// discart the first item if it's empty
+			if( path[i] === "") continue;
+			router = (path[i].charAt(0).toUpperCase() + path[i].slice(1));
+			// stop if we've found a router
+			if(typeof(APP.Routers[router]) == "function") break;
+		}
+		// call the router or fallback to the default
+		var controller = (router) ? new APP.Routers[router]() : new APP.Routers.Default();
+	}
+	
+	// Routers
+	APP.Routers.Default = Backbone.Router.extend({
 		data: {}, 
 		initialize: function() {
 			// every function that uses 'this' as the current object should be in here
