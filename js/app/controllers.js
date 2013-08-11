abd82938dd4d614986be8891c68926750f27f518
Makis Tracend <makis.tracend@gmail.com>
2013-08-11 02:20:00 -0700

    using Backbone.Model for data container

--- js/app/controllers.js
+++ js/app/controllers.js
@@ -1,25 +1,28 @@
-(function() {
+(function(_, Backbone) {
 
 	// Routers
 	APP.Routers.Default = APP.Router.extend({
-		data: {}, 
+		data: {},
 		initialize: function() {
 			// every function that uses 'this' as the current object should be in here
-			_.bindAll(this, 'index'); 
-			// uncomment this if using Google Analytics
-			// this.bind('all', this._trackPageview);
-			
-		}, 
+			_.bindAll(this, 'index');
+
+		},
 		routes: {
 			"": "index",
-		}, 
+		},
 		index: function(){
 			console.log("I'm in index");
-			// do something...
-			//APP.Collections.Main
-			//APP.Views.Main
+			// load data
+			this.data = new Backbone.Model({
+				// enter custom data here...
+			});
+			// init layout
+			this.layout = new APP.Layouts.Main({
+				data: this.data
+			});
 		}
-		
+
 	});
 
-})();
+})(this._, this.Backbone);
