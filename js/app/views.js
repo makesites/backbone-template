7e319dccf05fccc09244c9891e39b30c47030ce6
Makis Tracend <makis.tracend@gmail.com>
2013-08-11 02:15:26 -0700

    replacing main view with a layout

--- js/app/views.js
+++ js/app/views.js
@@ -1,42 +1,21 @@
-(function(_, Backbone) {
-	
+(function(_, Backbone, $) {
+
 	/* Main layout */
-	APP.Views.Main = APP.View.extend({
-		// the template file that's used as a resource for the markup
-		el: "#main", 
-		initialize: function(model, options){ 
-			
-			// every function that uses 'this' as the current object should be in here
-			_.bindAll(this, 'render', 'update', 'clickExternal'); 
-			
-			// get the data
-			this.model = model;
-			
-			// render the page
-			this.render();
-			
-		},
-		// Presentation View rendering
-		render: function(){
-			
-			// remove loading state
-			$("body").removeClass("loading");
-			
-			// return the object for reference
-			return this;
-		}, 
-		// Update the view when a new model is sent
-		update: function( model, options ){
-			
+	APP.Layouts.Main = APP.Layout.extend({
+		initialize: function( options ){
+
+			// custom code...
+
+			return APP.Layout.prototype.initialize.call( this, options );
 		}
 	});
-	
+
 	// Section views (duplicate as needed...)
 	APP.Views.Section = APP.View.extend({
-		el: "", 
-		events: {}, 
+		el: "",
+		events: {},
 		initialize: function(model, options){},
 		render: function(){}
 	});
-	
-})(this._, this.Backbone);
+
+})(this._, this.Backbone, this.$);
